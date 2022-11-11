027347fe30d4e57bbfb2487ebaa685ed8f7b97a2
DChernyh <sardimka@gmail.com>
2022-11-11 15:37:31 +0400

    playing :-)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-shit-code';
+export class AppComponent implements OnChanges, OnInit {
+  title: string = 'ng-shit-code';
+  users: string[] = ['Alex', 'John', 'Jane'];
+  isRed: boolean = false;
+  img_url!: string;
+  input_val: string = '';
+
+  constructor() {
+    setTimeout(() => {
+      this.img_url = 'https://picsum.photos/200';
+    }, 5000);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(this.input_val);
+  }
+
+  ngOnInit(): void {
+    // alert('Initialized');
+  }
+
+  onInput(event: any) {
+    this.input_val = event.target.value;
+  }
+
+  onClick(){
+    this.isRed = !this.isRed;
+  }
 }
