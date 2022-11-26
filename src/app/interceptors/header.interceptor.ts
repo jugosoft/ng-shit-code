aac166d3439680758312689b3c6aad3385ab114a
jugosoft <sardimka@gmail.com>
2022-11-26 20:55:57 +0400

    trying to cors

--- src/app/interceptors/header.interceptor.ts
+++ src/app/interceptors/header.interceptor.ts
@@ -5,8 +5,9 @@ export class HeaderInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // Добавим заголовок и ограничим число дел сотней
         const request = req.clone({
-            headers: req.headers.append('custom-header', 'customHeaderValue'),
-            params: req.params.get('_limit') ? req.params.set('_limit', '100') : req.params,
+            headers: req.headers.append('custom-header', 'customHeaderValue')
+                                .append('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE'),
+            params: req.params.get('_limit') && req.method === 'GET' ? req.params.set('_limit', '100') : req.params,
         })
         return next.handle(request).pipe(
             tap(event => {
